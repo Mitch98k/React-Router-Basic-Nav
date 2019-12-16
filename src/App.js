716eebbf2bab72d1a5ef3d653668d7ca3a8acb5b
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2019-12-16 14:46:35 -0700

    I got the navigation routes working.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
+import {Route} from "react-router-dom";
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
     <Navigation />
-  </div>
+  
+  <Route exact path = "/">
+    <home />
+  </Route>
+
+  <Route path = "/about">
+    <about />
+  </Route>
+
+  <Route path = "/contact">
+    <contact />
+  </Route>
+</div>
 );
 
 export default App;
